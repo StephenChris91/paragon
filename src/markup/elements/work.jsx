524e9c6fe781ecc04f96488662648531235c9500
Stephen Chris <46078420+StephenChris91@users.noreply.github.com>
2023-05-14 19:35:26 +0100

    created partners logo section

--- src/markup/elements/work.jsx
+++ src/markup/elements/work.jsx
@@ -12,7 +12,7 @@ const ourProcess = () => {
 	return (
 		<>
 
-			<section className="section-area section-sp5 work-area" style={{ backgroundImage: "url(" + bg1 + ")", backgroundRepeat: " no-repeat", backgroundPosition: "center", backgroundSize: "100%" }}>
+			<section className="section-area section-sp5 work-area" style={{ width: '100', backgroundImage: "url(" + bg1 + ")", backgroundRepeat: " no-repeat", backgroundPosition: "center", backgroundSize: "100%" }}>
 				<div className="container-sm">
 					<div className="heading-bx text-center">
 						<h6 className="title-ext text-secondary">Our Process</h6>
@@ -22,8 +22,9 @@ const ourProcess = () => {
 						<div className="col-lg-4 col-sm-6 mb-30">
 							<div className="work-bx">
 								<div className="work-num-bx">01</div>
+								
 								<div className="work-content">
-									<h5 className="title text-secondary mb-10">Make Appointment</h5>
+									<h5 className="title text-secondary mb-10">Book An Appointment</h5>
 									<p>Schedule and appointment with with on of our doctors by using the booking form on our website.</p>
 								</div>
 								<Link to="/booking" className="btn btn-primary light">View More <i className="btn-icon-bx fas fa-chevron-right"></i></Link>
@@ -33,7 +34,7 @@ const ourProcess = () => {
 							<div className="work-bx active">
 								<div className="work-num-bx">02</div>
 								<div className="work-content">
-									<h5 className="title text-secondary mb-10">Attend Appointment</h5>
+									<h5 className="title text-secondary mb-10">Talk To A Doctor</h5>
 									<p>Attend your scheduled appointment with our doctors where we would diagnose your ailment by carrying out necessary tests and sessions</p>
 								</div>
 								<Link to="/services" className="btn btn-primary light">View More <i className="btn-icon-bx fas fa-chevron-right"></i></Link>
@@ -41,21 +42,31 @@ const ourProcess = () => {
 						</div>
 						<div className="col-lg-4 col-sm-6 mb-30">
 							<div className="work-bx">
-								<div className="work-num-bx">03</div>
+								<div className="work-num-bx">01</div>
 								<div className="work-content">
-									<h5 className="title text-secondary mb-10">Begin Treatment</h5>
-									<p>Begin your treatment immediately and keep in touch with our Doctors to monitor your progress.</p>
+									<h5 className="title text-secondary mb-10">Book An Appointment</h5>
+									<p>Schedule and appointment with with on of our doctors by using the booking form on our website.</p>
 								</div>
-								<Link to="/contact-us" className="btn btn-primary light">View More <i className="btn-icon-bx fas fa-chevron-right"></i></Link>
+								<Link to="/booking" className="btn btn-primary light">View More <i className="btn-icon-bx fas fa-chevron-right"></i></Link>
 							</div>
 						</div>
-						<Link to="/booking" className="btn btn-primary col-lg-4">Take A Tour</Link>
+						<div className="col-lg-4 col-sm-6 mb-30">
+							<div className="work-bx active">
+								<div className="work-num-bx">02</div>
+								<div className="work-content">
+									<h5 className="title text-secondary mb-10">Talk To A Doctor</h5>
+									<p>Attend your scheduled appointment with our doctors where we would diagnose your ailment by carrying out necessary tests and sessions</p>
+								</div>
+								<Link to="/services" className="btn btn-primary light">View More <i className="btn-icon-bx fas fa-chevron-right"></i></Link>
+							</div>
+						</div>
 					</div>
+					<Link to="/booking" className="btn btn-primary col-lg-4">Take A Tour</Link>
 				</div>
 				<img className="pt-img1 animate1" src={ptImg1} alt="" />
 				<img className="pt-img2 animate2" src={ptImg2} alt="" />
 				<img className="pt-img3 animate3" src={ptImg3} alt="" />
-			</section>
+			</section >
 
 		</>
 	);
